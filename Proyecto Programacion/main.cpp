b298121236bb328674e1fe411f86d5b629d7cf3f
1klasT <nicolas.mancilla.js@gmail.com>
2023-07-04 15:59:55 -0300

    Se agrego el Proyecto

--- Proyecto Programacion/main.cpp
+++ Proyecto Programacion/main.cpp
@@ -4,17 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int ANCHO_PANTALLA = 800;
-const int ALTURA_PANTALLA = 600;
+#define cuadro_matriz 40
+#define ANCHO_PERSONAJE 42
 
-const int TAMANO_PERSONAJE = 50;
+const int ANCHO_PANTALLA = 1600;
+const int ALTURA_PANTALLA = 920;
+
+const int TAMANO_PERSONAJE = 69;
 const int VELOCIDAD_PERSONAJE = 5;
 
-const int ANCHO_MAPA = 16;
-const int ALTURA_MAPA = 12;
-
-int personaje_x;
-int personaje_y;
+const int ANCHO_MAPA = 40;
+const int ALTURA_MAPA = 23;
 
 bool key_left = false;
 bool key_right = false;
@@ -23,8 +23,18 @@ bool key_down = false;
 
 int mapa[ALTURA_MAPA][ANCHO_MAPA];
 
+struct personaje {
+     int personaje_x=0, personaje_y=0;
+
+};
+
+struct personaje jugador;
+
+ALLEGRO_BITMAP* imagen_personaje;
 ALLEGRO_BITMAP* imagen_bloque;
 ALLEGRO_BITMAP* imagen_fondo;
+ALLEGRO_BITMAP* imagen_piso;
+ALLEGRO_BITMAP* imagen_fuego;
 
 void cargarMapa(const char* nombrearchivo) {
     int x, y;
@@ -46,13 +56,30 @@ void cargarMapa(const char* nombrearchivo) {
 
     fclose(fdata);
 }
+void cargarPersonaje() {
+    imagen_personaje = al_load_bitmap("player.png");
+}
 
 void cargarImagenBloque() {
     imagen_bloque = al_load_bitmap("bloque.png");
 }
 
+void cargarImagenPiso() {
+    imagen_piso = al_load_bitmap("grass.png");
+}
+
 void cargarImagenFondo() {
     imagen_fondo = al_load_bitmap("wallpaper.png");
+}
+
+void cargarImagenFuego() {
+    imagen_fuego = al_load_bitmap("fuego.png");
+}
+
+void cargarGravedad() {
+    if (mapa[(jugador.personaje_y + TAMANO_PERSONAJE) / cuadro_matriz][jugador.personaje_x / cuadro_matriz] == 0 && mapa[(jugador.personaje_y + TAMANO_PERSONAJE) / cuadro_matriz][(jugador.personaje_x + ANCHO_PERSONAJE) / cuadro_matriz] == 0) {
+        jugador.personaje_y+=2;
+    }
 }
 
 void actualizarPosicionPersonaje() {
@@ -61,20 +88,16 @@ void actualizarPosicionPersonaje() {
 
 
     if (key_left) {
-        nueva_x = personaje_x - VELOCIDAD_PERSONAJE;
-        personaje_x = nueva_x;
+        nueva_x = jugador.personaje_x - VELOCIDAD_PERSONAJE;
+        jugador.personaje_x = nueva_x;
     }
     if (key_right) {
-        nueva_x = personaje_x + VELOCIDAD_PERSONAJE;
-        personaje_x = nueva_x;
+        nueva_x = jugador.personaje_x + VELOCIDAD_PERSONAJE;
+        jugador.personaje_x = nueva_x;
     }
     if (key_up) {
-        nueva_y = personaje_y - VELOCIDAD_PERSONAJE;
-        personaje_y = nueva_y;
-    }
-    if (key_down) {
-        nueva_y = personaje_y + VELOCIDAD_PERSONAJE;
-        personaje_y = nueva_y;
+        nueva_y = jugador.personaje_y - VELOCIDAD_PERSONAJE;
+        jugador.personaje_y = nueva_y;
     }
 }
 
@@ -92,11 +115,15 @@ int main() {
     al_register_event_source(event_queue, al_get_keyboard_event_source());
 
     cargarMapa("mapa.txt");
+    cargarGravedad();
+    cargarPersonaje();
     cargarImagenBloque();
+    cargarImagenPiso();
+    cargarImagenFuego();
     cargarImagenFondo();
 
-    personaje_x = 0;
-    personaje_y = ALTURA_PANTALLA - TAMANO_PERSONAJE;
+    jugador.personaje_x = 0;
+    jugador.personaje_y = ALTURA_PANTALLA - TAMANO_PERSONAJE - 42;
 
     corriendo = true;
 
@@ -107,6 +134,7 @@ int main() {
     while (corriendo) {
         ALLEGRO_EVENT event;
         al_wait_for_event(event_queue, &event);
+        cargarGravedad();
 
         if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             corriendo = false;
@@ -144,29 +172,34 @@ int main() {
             }
         }
         al_clear_to_color(al_map_rgb(0, 0, 0));
-        al_draw_scaled_bitmap(imagen_fondo, 0, 0, al_get_bitmap_width(imagen_fondo), al_get_bitmap_height(imagen_fondo), 0, 0, ANCHO_PANTALLA, ALTURA_PANTALLA, 0);
+        // printf("\nCoordenadas: (%d, %d)", personaje_x/50, (personaje_y + TAMANO_PERSONAJE)/50);
+        al_draw_bitmap(imagen_fondo, 0, 0, 0);
         
         for (y = 0; y < ALTURA_MAPA; ++y) {
             for (x = 0; x < ANCHO_MAPA; ++x) {
                 if (mapa[y][x] == 1) {
-                    al_draw_scaled_bitmap(imagen_bloque, 0, 0, al_get_bitmap_width(imagen_bloque), al_get_bitmap_height(imagen_bloque),
-                        x * TAMANO_PERSONAJE, y * TAMANO_PERSONAJE, TAMANO_PERSONAJE, TAMANO_PERSONAJE, 0);
+                    al_draw_bitmap(imagen_bloque, cuadro_matriz * x, cuadro_matriz * y, 0);
+                }
+                else if (mapa[y][x] == 2) {
+                    al_draw_bitmap(imagen_piso, cuadro_matriz * x, cuadro_matriz * y, 0);
+                }
+                else if (mapa[y][x] == 3) {
+                    al_draw_bitmap(imagen_fuego, cuadro_matriz * x, cuadro_matriz * y, 0);
                 }
             }
         }
-
+           
         actualizarPosicionPersonaje();
-        al_draw_filled_rectangle(personaje_x, personaje_y, personaje_x + TAMANO_PERSONAJE, personaje_y + TAMANO_PERSONAJE, al_map_rgb(255, 255, 255));
+        al_draw_bitmap(imagen_personaje, jugador.personaje_x, jugador.personaje_y, 0);
         al_flip_display();
     }
 
     al_destroy_display(display);
     al_destroy_event_queue(event_queue);
+    al_destroy_bitmap(imagen_personaje);
     al_destroy_bitmap(imagen_bloque);
     al_destroy_bitmap(imagen_fondo);
     al_destroy_timer(timer);
-
-    printf("Mensaje nuevo");
 
     return 0;
 }
